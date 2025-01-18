a2990ab32b72377045c5e9f9f8e9bc0396f18575
Mithaverse <ens.sryldz@gmail.com>
2025-01-18 21:40:41 +0300

    Frontend/ NotFound component added.

--- Frontend/App.Client/src/api/request.ts
+++ Frontend/App.Client/src/api/request.ts
@@ -13,7 +13,7 @@ axios.interceptors.response.use(response => {
             break;
         case 401: toast.error(data.title);
             break;
-        case 404: toast.error(data.title);
+        case 404: router.navigate("/not-found");
             break;
         case 500: router.navigate("/server-error", { state: { error: data, status: status } })
             break;
