8cc866c7d389577c51ee7490f9511058a9f1c91c
Mithaverse <ens.sryldz@gmail.com>
2025-02-01 20:39:50 +0300

    Frontend/ Cart context-1

---
+++ Frontend/App.Client/src/context/CartContext.tsx
@@ -0,0 +1,25 @@
+import { createContext, PropsWithChildren, useState } from "react";
+import { Cart } from "../model/ICart";
+
+interface CartContextValue {
+    cart: Cart | null;
+    setCart: (cart: Cart) => void;
+    deleteItem: (productId: number, quantity: number) => void;
+}
+
+export const CartContext = createContext<CartContextValue | undefined>(undefined)
+
+export function CartContextProvider({ children }: PropsWithChildren<any>) {
+    const [cart, setCart] = useState<Cart | null>(null);
+
+    function deleteItem(productId: number, quantity: number) {
+
+    }
+
+    return (
+        <CartContext.Provider value={{ cart, setCart, deleteItem }}>
+            {children}
+        </CartContext.Provider>
+    )
+
+}
