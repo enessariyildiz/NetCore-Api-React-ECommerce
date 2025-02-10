e9385406e187d92c77d3d9595435b52c45c7a163
Mithaverse <ens.sryldz@gmail.com>
2025-02-10 22:43:22 +0300

    Frontend/ Updating cart items done.

--- Frontend/App.Client/src/pages/cart/ShoppingCartPage.tsx
+++ Frontend/App.Client/src/pages/cart/ShoppingCartPage.tsx
@@ -7,26 +7,26 @@ import request from "../catalog/request";
 
 export default function ShoppingCartPage() {
     const { cart, setCart } = useCartContext;
-    const [loading, setLoading] = useState(false);
+    const [status, setStatus] = useState({ loading: false, id: "" });
 
 
-    function handleAddItem(productId: number) {
-        setLoading(true);
+    function handleAddItem(productId: number, id: string) {
+        setStatus({ loading: true, id: id });
 
         request.Cart.addItem(productId)
             .then(cart => setCart(cart))
             .catch(error => console.log(error))
-            .finally(() => setLoading(false));
+            .finally(() => setStatus({ loading: false, id: "" }));
+
     }
 
-    function handleDeleteItem(productId: number, quantity = 1) {
-        setLoading(true);
+    function handleDeleteItem(productId: number, id: string, quantity = 1) {
+        setStatus({ loading: true, id: id });
 
         request.Cart.deleteItem(productId)
             .then(cart => setCart(cart))
             .catch(error => console.log(error))
-            .finally(() => setLoading(false));
-
+            .finally(() => setStatus({ loading: false, id: "" }));
     }
 
     if (cart?.cartItems.lenght === 0) return <Alert severity="warning">Shopping cart is empty!</Alert>;
@@ -44,7 +44,7 @@ export default function ShoppingCartPage() {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {cart.cartItems.map((item) => (
+                    {cart?.cartItems.map((item) => (
                         <TableRow key={item.productId}>
                             <TableCell component="th" scope="row">
                                 <img src={`https://localhost:7244/img/${item.imageUrl}`} style={{ height: 60 }} />
@@ -54,17 +54,20 @@ export default function ShoppingCartPage() {
                             </TableCell>
                             <TableCell align="right">${item.price.toFixed(2)}</TableCell>
                             <TableCell align="right">
-                                <LoadingButton loading={loading} onClick={() => handleAddItem(item.productId)}>
+                                <LoadingButton loading={status.loading && status.id === "add" + item.productId}
+                                    onClick={() => handleAddItem(item.productId, "add" + item.productId)}>
                                     <AddCircleOutline />
                                 </LoadingButton>
                                 {item.quantity}
-                                <LoadingButton loading={loading} onClick={() => handleDeleteItem(item.productId)}>
+                                <LoadingButton loading={status.loading && status.id === "del" + item.productId}
+                                    onClick={() => handleDeleteItem(item.productId, "del" + item.productId)}>
                                     <RemoveCircleOutline />
                                 </LoadingButton>
                             </TableCell>
                             <TableCell align="right">${(item.price * item.quantity).toFixed(2)}</TableCell>
                             <TableCell align="right">
-                                <IconButton color="error" loading={loading} onClick={() => handleDeleteItem(item.productId, item.quantity)}>
+                                <IconButton color="error" loading={status.loading && status.id === "del_all" + item.productId}
+                                    onClick={() => handleDeleteItem(item.productId, "del_all" + item.productId, item.quantity)}>
                                     <Delete />
                                 </IconButton>
                             </TableCell>
