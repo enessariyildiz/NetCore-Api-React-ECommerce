bc90600e07f8e0887eb9efda570b0c17fa2c4e53
Mithaverse <ens.sryldz@gmail.com>
2025-02-14 23:04:43 +0300

    Frontend/ Create a redux state slice.

--- Frontend/App.Client/src/store/store.ts
+++ Frontend/App.Client/src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "../features/counter/counterSlice";
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        counter: counterSlice.reducer
+    }
 })
 
 export type RootState = ReturnType<typeof store.getState>
