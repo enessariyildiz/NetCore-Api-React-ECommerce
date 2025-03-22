db39afd3d6af8d459ed9608b64f2c901037f0566
Mithaverse <ens.sryldz@gmail.com>
2025-03-22 18:29:38 +0300

    Frontend/ Stepper component added.

--- Frontend/App.Client/src/features/checkout/CheckoutPage.tsx
+++ Frontend/App.Client/src/features/checkout/CheckoutPage.tsx
@@ -1,23 +1,87 @@
-import { Info } from "@mui/icons-material";
-import { Grid2, Paper } from "@mui/material";
+import { ChevronLeftRounded, ChevronRightRounded, Info } from "@mui/icons-material";
+import { Box, Button, Grid2, Paper, Step, StepLabel, Stepper } from "@mui/material";
 import AddressForm from "./AddressForm";
 import Review from "./Review";
 import PaymentForm from "./PaymentForm";
+import { useState } from "react";
+
+const step = ["Delivery Information", "Payment", "Order Summary"]
+
+function getStepContent(step: number) {
+
+    switch (step) {
+        case 0:
+            return <AddressForm />
+        case 1:
+            return <PaymentForm />
+        case 2:
+            return <Review />
+        default:
+            throw new Error("Unknow a step");
+    }
+}
 
 export default function CheckoutPage() {
+
+    const [activeStep, setActiveStep] = useState(0);
+
+    function handleNext() {
+        setActiveStep(activeStep + 1);
+    }
+
+    function handlePrevious() {
+        setActiveStep(activeStep - 1);
+    }
+
     return (
         <Paper>
-            <Grid2 container sx={{ p: 4 }} spacing={2} >
+            <Grid2 container sx={{ p: 4 }} spacing={4} >
                 <Grid2 size={4}>
                     <Info />
                 </Grid2>
                 <Grid2 size={8}>
-                    <AddressForm />
-                    <PaymentForm />
-                    <Review />
+                    <Box>
+                        <Stepper activeStep={activeStep} sx={{ height: 40 }}>
+                            {step.map((label) => (
+                                <Step key={label}>
+                                    <StepLabel>{label}</StepLabel>
+                                </Step>
+                            ))}
+                        </Stepper>
+                    </Box>
+                    <Box>
+                        {activeStep === step.length ? (
+                            <h2>Order Complated</h2>
+                        ) : (
+                            <>
+                                {getStepContent(activeStep)}
+                                <Box>
+                                    <Box sx={
+                                        [
+                                            {
+                                                display: "flex"
+                                            },
+                                            activeStep !== 0
+                                                ? { justifyContent: "space-between" }
+                                                : { justifyContent: "flex-end" }
+                                        ]
 
+                                    }>
+                                        {
+                                            activeStep !== 0 &&
+                                            <Button startIcon={<ChevronRightRounded />} variant="contained" onClick={handlePrevious}>Back</Button>
+
+                                        }
+
+                                        <Button startIcon={<ChevronLeftRounded />} variant="contained" onClick={handleNext}>Next</Button>
+
+                                    </Box>
+                                </Box>
+                            </>
+                        )}
+                    </Box>
                 </Grid2>
             </Grid2>
-        </Paper>
+        </Paper >
     );
 };
