2a58c01e5b24431ebea730d8c5fd4bea5b178ffc
Mithaverse <ens.sryldz@gmail.com>
2025-04-03 22:44:40 +0300

    Frontend/ Order details done

--- Frontend/App.Client/src/features/orders/OrderList.tsx
+++ Frontend/App.Client/src/features/orders/OrderList.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { Order } from "../../model/IOrder";
-import { Button, CircularProgress, Dialog, DialogContent, DialogTitle, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import request from "../../api/request";
 import { currencyTRY } from "../../utils/formatCurrency";
 import { ArrowRightOutlined } from "@mui/icons-material";
 import CloseIcon from '@mui/icons-material/Close';
+
 
 const orderStatus = ["Pending", "Approved", "PaymentFailed", "Complated"];
 
@@ -14,6 +15,10 @@ export default function OrderList() {
 
     const [selectOrder, setSelecetOrder] = useState<Order | null>(null);
     const [open, setOpen] = useState(false);
+
+    const subTotal = selectOrder?.orderItems.reduce((total, item) => total + (item.quantity * item.price), 0) ?? 0;
+    const tax = subTotal * 0.2;
+    const total = subTotal + tax;
 
     function handleDialogOpen(order: Order) {
 
@@ -68,7 +73,9 @@ export default function OrderList() {
                 </Table>
             </TableContainer>
 
-            <Dialog onClose={handleDialogClose} open={open} fullWidth maxWidth="lg">
+            <Dialog onClose={handleDialogClose} open={open} fullWidth maxWidth="lg"
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description">
                 <DialogTitle>
                     Order Number : # {selectOrder?.id}
                 </DialogTitle>
@@ -82,7 +89,46 @@ export default function OrderList() {
                         <Typography gutterBottom>{selectOrder?.phone}</Typography>
                         <Typography gutterBottom>{selectOrder?.addressLine} / {selectOrder?.city}</Typography>
                     </Paper>
+                    <Table>
+                        <TableHead id="alert-dialog-title">
+                            <TableRow>
+                                <TableCell></TableCell>
+                                <TableCell></TableCell>
+                                <TableCell align="right">Price</TableCell>
+                                <TableCell align="right">Number</TableCell>
+                                <TableCell align="right">Total</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {selectOrder?.orderItems.map((item) => (
+                                <TableRow key={item.id}>
+                                    <TableCell>
+                                        <img src={`https://localhost:7244/img/${item.productImage}`} style={{ height: 60 }} />
+                                    </TableCell>
+                                    <TableCell align="right">{item.productName}</TableCell>
+                                    <TableCell align="right">{currencyTRY.format(item.price)}</TableCell>
+                                    <TableCell align="right">{item.quantity}</TableCell>
+                                    <TableCell align="right">{currencyTRY.format(item.price * item.quantity)}</TableCell>
+                                </TableRow>
+                            ))}
+                            <TableRow>
+                                <TableCell align="right" colSpan={4}>Sub Total</TableCell>
+                                <TableCell align="right" colSpan={4}>{currencyTRY.format(subTotal)}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="right" colSpan={4}>Tax</TableCell>
+                                <TableCell align="right" colSpan={4}>{currencyTRY.format(tax)}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="right" colSpan={4}>Total</TableCell>
+                                <TableCell align="right" colSpan={4}>{currencyTRY.format(total)}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
                 </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleDialogClose}>Close</Button>
+                </DialogActions>
             </Dialog>
         </>
     )
