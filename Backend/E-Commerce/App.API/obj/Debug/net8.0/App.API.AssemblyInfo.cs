b167d3232befe6fbdc81d1b221af1dbe78bdaa62
Mithaverse <ens.sryldz@gmail.com>
2025-04-04 22:16:51 +0300

    Backend/ Payment API added.

--- Backend/E-Commerce/App.API/obj/Debug/net8.0/App.API.AssemblyInfo.cs
+++ Backend/E-Commerce/App.API/obj/Debug/net8.0/App.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+966fd18981f733732027b762088c3a406532edb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a58c01e5b24431ebea730d8c5fd4bea5b178ffc")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
